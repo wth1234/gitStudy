3c27c6bf84b2a58fe8556ab5a9bda74a30af547a
王天昊 <wangthwork@163.com>
2022-08-07 17:15:04 +0800

    test git

---
+++ testgit.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class testgit {
+
+    public static void main(String[] args) {
+        System.out.println("this is git");
+    }
+}
